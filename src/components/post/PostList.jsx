23977b2f5a68e3744bd2ad581a2f4f0352550a58
Zed-snek <ed.savchuker@gmail.com>
2023-08-24 14:16:58 +0200

    New hook useObserver.js. PostList uses scroll pagination with useObserver.js

--- src/components/post/PostList.jsx
+++ src/components/post/PostList.jsx
@@ -1,15 +1,18 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import {useFetching} from "../../hooks/useFetching";
 import style from "./PostList.module.css";
 import MySyncLoader from "../UI/loaders/MySyncLoader";
 import PostService from "../../API/PostService";
 import MyMessage from "../UI/message/MyMessage";
 import PostItem from "./PostItem";
+import {useObserver} from "../../hooks/useObserver";
 
 function PostList({loadType, entityName}) { //loadType: HOME / COMMUNITY / USER
 
     const [pageNumber, setPageNumber] = useState(0)
     const [data, setData] = useState([])
+    const [canLoad, setCanLoad] = useState(true)
+    const lastElement = useRef()
 
     const [fetchPosts, isFetchLoading, fetchError] = useFetching(async () => {
         let responseData
@@ -24,13 +27,19 @@ function PostList({loadType, entityName}) { //loadType: HOME / COMMUNITY / USER
                 responseData = await PostService.getPostsForUserPage(entityName, pageNumber)
                 break
         }
+        if (responseData.length === 0)
+            setCanLoad(false)
         setData(prev => [...prev, ...responseData])
-        console.log(responseData)
+    })
+
+    useObserver(lastElement, canLoad, isFetchLoading, () => {
+        setPageNumber(prev => prev + 1)
     })
 
     useEffect(() => {
         fetchPosts()
     }, [pageNumber])
+
 
     return (
         <div className={style.main}>
@@ -65,11 +74,14 @@ function PostList({loadType, entityName}) { //loadType: HOME / COMMUNITY / USER
                 )
                 : <></>
             }
+            <div ref={lastElement} style={{height: 20}}>
 
+            </div>
             <div className={style.loaderDiv}>
                 <MyMessage className={style.error}>{fetchError}</MyMessage>
                 <MySyncLoader loading={isFetchLoading} />
             </div>
+
         </div>
     );
 }
